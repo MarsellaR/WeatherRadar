652b15b66336a0babe913f0ae6b15ac54a59e67b
Marsella/R <rozic.marsela@gmail.com>
2022-11-09 11:11:15 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -33,14 +33,16 @@ function App() {
         const forecastWeather = fetch(
           `${weather}/forecast?lat=${city[0]?.latitude}&lon=${city[0]?.longitude}&appid=${weather_key}&units=metric`
         );
-        Promise.all([currentWeather, forecastWeather]).then(
-          async (response) => {
+        Promise.all([currentWeather, forecastWeather])
+          .then(async (response) => {
             const weatherResponse = await response[0].json();
             const forecastResponse = await response[1].json();
             setCurrentWeather(weatherResponse);
             setForecastWeather(forecastResponse);
-          }
-        );
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       }
       if (locator !== "") {
         setCity("");
@@ -51,15 +53,16 @@ function App() {
         const forecastWeather2 = fetch(
           `${weather}/forecast?lat=${locator?.latitude}&lon=${locator?.longitude}&appid=${weather_key}&units=metric`
         );
-        Promise.all([currentWeather2, forecastWeather2]).then(
-          async (response) => {
+        Promise.all([currentWeather2, forecastWeather2])
+          .then(async (response) => {
             const weatherResponse2 = await response[0].json();
             const forecastResponse2 = await response[1].json();
-
             setCurrentWeather(weatherResponse2);
             setForecastWeather(forecastResponse2);
-          }
-        );
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       }
     };
     fetchData();
@@ -101,7 +104,6 @@ function App() {
           {currentWeather && (
             <CurrentWeather weatherData={currentWeather} city={city} />
           )}
-
           <div className="weather-display">
             {displayWeather()}
             {currentWeather && <Higlight currentWeather={currentWeather} />}
@@ -112,7 +114,6 @@ function App() {
           {currentWeather && (
             <CurrentWeather weatherData={currentWeather} city={city} />
           )}
-
           <div className="weather-display">
             {currentWeather && <Higlight currentWeather={currentWeather} />}
           </div>
